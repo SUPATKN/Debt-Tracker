ccb302aa07b5a35e5ee3de1f0105a11dc2cfaefe
SUPATKN <yacht1400@gmail.com>
2025-04-11 23:17:02 +0700

    Backend : add init backend

---
+++ backend/Debt-Tracker-backend/src/main/java/com/supatkon/Debt_Tracker_backend/DebtTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.supatkon.Debt_Tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebtTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/Debt-Tracker-backend/src/test/java/com/supatkon/Debt_Tracker_backend/DebtTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supatkon.Debt_Tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebtTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
